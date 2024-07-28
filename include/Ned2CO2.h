99fb7be43f4049132d1f78c5a647d43564bf63b0
Eual11 <uchihaeual11@gmail.com>
2024-07-29 01:08:44 +0300

    on progress implementation of the PPU

---
+++ include/Ned2CO2.h
@@ -0,0 +1,45 @@
+#ifndef __NED2CO2__H
+#define __NED2CO2__H
+
+// Ned's PPU: 2C02
+#include <cstdint>
+#include <memory>
+#include <stdint.h>
+namespace NedNes {
+class NedBus;
+class Ned2C02 {
+
+public:
+  // cpu read and writes
+  // these methods are called from the bus handle read and write signals from
+  // cpu
+
+  Ned2C02();
+  uint8_t cpuRead(uint16_t addr);
+  void cpuWrite(uint16_t addr, uint8_t data);
+
+  // PPU read and writes
+
+  uint8_t ppuRead(uint8_t addr);
+  void ppuWrite(uint16_t addr, uint8_t data);
+
+  // connecting to cartage and BUS
+
+  // scanlines and cycles
+
+  std::shared_ptr<NedBus> bus;
+
+  void connectBus(std::shared_ptr<NedBus>);
+
+private:
+  unsigned int cycles = 0;
+  unsigned int scanlines = 0;
+
+  uint8_t paletteTable[32];   // 32Byte  palette
+  uint8_t nameTable[4][1024]; // name table used to store background information
+  uint8_t patternTable[2][1024]; // pattern table, this is located inside th chr
+                                 // rom but i am storing it here for now
+};
+}; // namespace NedNes
+
+#endif
---
+++ src/Ned2CO2.cpp
@@ -0,0 +1,103 @@
+#include "../include/Ned2CO2.h"
+
+NedNes::Ned2C02::Ned2C02() {
+  // TODO: load the palette tables
+  //
+  cycles = 0;
+  scanlines = 0;
+}
+
+uint8_t NedNes::Ned2C02::cpuRead(uint16_t addr) {
+  switch (addr) {
+  case 0x00: {
+    // PPU Control Register
+    break;
+  }
+
+  case 0x01: {
+    // PPU Mask Registers
+    break;
+  }
+
+  case 0x02: {
+    // PPU Status Register
+    break;
+  }
+
+  case 0x03: {
+    // OAM Address Register
+    break;
+  }
+  case 0x04: {
+    // OAM Data Register
+    break;
+  }
+  case 0x05: {
+    // PPU Scroll register
+    break;
+  }
+  case 0x06: {
+    // PPU Address Register
+    break;
+  }
+  case 0x07: {
+    // PPU Data Register
+  }
+  }
+
+  return 0x00;
+}
+void NedNes::Ned2C02::cpuWrite(uint16_t addr, uint8_t data) {
+  switch (addr) {
+  case 0x00: {
+    // PPU Control Register
+    break;
+  }
+
+  case 0x01: {
+    // PPU Mask Registers
+    break;
+  }
+
+  case 0x02: {
+    // PPU Status Register
+    break;
+  }
+
+  case 0x03: {
+    // OAM Address Register
+    break;
+  }
+  case 0x04: {
+    // OAM Data Register
+    break;
+  }
+  case 0x05: {
+    // PPU Scroll register
+    break;
+  }
+  case 0x06: {
+    // PPU Address Register
+    break;
+  }
+  case 0x07: {
+    // PPU Data Register
+  }
+  }
+}
+
+uint8_t NedNes::Ned2C02::ppuRead(uint8_t addr) {
+
+  addr &= 0x3FFF;
+  uint8_t data = 0x00;
+
+  // do something idk yet lmao
+  //
+  return data;
+}
+void NedNes::Ned2C02::ppuWrite(uint16_t addr, uint8_t data) {
+
+  // do something idk yet lmao
+}
+
+void NedNes::Ned2C02::connectBus(std::shared_ptr<NedBus> _bus) { bus = _bus; }
