8849ff767106362a18808c48a7b78b03f484148c
Eual11 <uchihaeual11@gmail.com>
2024-07-29 01:09:09 +0300

    Adding PPU and cartige to the bus making it look more like NES from just a 6502 bus

--- include/NedBus.h
+++ include/NedBus.h
@@ -1,5 +1,8 @@
 #pragma once
+#include "Ned2CO2.h"
+#include "NedCartridge.h"
 #include <array>
+#include <memory>
 #include <stdint.h>
 #include <vector>
 
@@ -11,13 +14,19 @@ class NedBus {
   // conencted devices
 private:
   Ned6502 *cpu;
-  // 64KB fake ram
-  std::array<uint8_t, 1024 * 64> ram;
+  std::shared_ptr<Ned2C02> ppu;
+  std::shared_ptr<NedCartrdige> cart;
+  //  a whooping 2KB of Nes ram!
+  std::array<uint8_t, 1024 * 2> ram;
 
 public:
   NedBus();
-  void write(uint16_t addr, uint8_t data);
-  uint8_t read(uint16_t addr);
-  void ConnectCpu(Ned6502 *_cpu);
+  void cpuWrite(uint16_t addr, uint8_t data);
+  uint8_t cpuRead(uint16_t addr);
+  uint8_t ppuRead(uint16_t addr);
+  void ppuWrite(uint16_t addr, uint8_t data);
+  void connectCpu(Ned6502 *_cpu);
+  void connectPpu(std::shared_ptr<Ned2C02>);
+  void connectCartridge(std::shared_ptr<NedCartrdige>);
 };
 } // namespace NedNes
--- src/NedBus.cpp
+++ src/NedBus.cpp
@@ -7,17 +7,27 @@ NedNes::NedBus::NedBus() { std::fill(std::begin(ram), std::end(ram), 0x00); }
 // Reading from memory, the NedBus is 16 bit wide bus so it is capable of
 // addressing 64Kb of memory
 
-uint8_t NedNes::NedBus::read(uint16_t addr) {
-  if (addr >= 0x0000 && addr <= 0xFFFF) {
-    return ram[addr];
+uint8_t NedNes::NedBus::cpuRead(uint16_t addr) {
+  if (addr >= 0x0000 && addr <= 0x1FFF) {
+    // mirroring the cpu read
+    return ram[addr & 0x7FF];
+  }
+  if (addr >= 0x2000 && addr <= 0x3FFF) {
+    return ppu->cpuRead(addr & 0x2000);
   }
   return 0x00;
 }
 // Writing to the bus, currently we only have the ram so we write to it
-void NedNes::NedBus::write(uint16_t addr, uint8_t val) {
-  if (addr >= 0x0000 && addr <= 0xFFFF) {
-    ram[addr] = val;
+void NedNes::NedBus::cpuWrite(uint16_t addr, uint8_t val) {
+  if (addr >= 0x0000 && addr <= 0x1FFF) {
+    // mirroring the cpu write
+    ram[addr & 0x7FF] = val;
+  }
+  if (addr >= 0x2000 && addr <= 0x3FFF) {
+    ppu->cpuWrite(addr & 0x2000, val);
   }
 }
 // connecting a cpu to the bus
-void NedNes::NedBus::ConnectCpu(NedNes::Ned6502 *_cpu) { cpu = _cpu; }
+void NedNes::NedBus::connectCpu(NedNes::Ned6502 *_cpu) { cpu = _cpu; }
+
+void NedNes::NedBus::connectPpu(std::shared_ptr<Ned2C02> _ppu) { ppu = _ppu; }
