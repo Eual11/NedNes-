aa9b63b525424c582349e0587108eb337e623b08
Eual11 <uchihaeual11@gmail.com>
2024-08-04 22:32:25 +0300

    Updating BUS

--- src/NedBus.cpp
+++ src/NedBus.cpp
@@ -8,25 +8,30 @@ NedNes::NedBus::NedBus() { std::fill(std::begin(ram), std::end(ram), 0x00); }
 // addressing 64Kb of memory
 
 uint8_t NedNes::NedBus::cpuRead(uint16_t addr) {
-  if (addr >= 0x0000 && addr <= 0x1FFF) {
+
+  uint8_t data = 0x00;
+  if (cart->cpuRead(addr, data)) {
+  } else if (addr >= 0x0000 && addr <= 0x1FFF) {
     // mirroring the cpu read
-    return ram[addr & 0x7FF];
+    data = ram[addr & 0x7FF];
+  } else if (addr >= 0x2000 && addr <= 0x3FFF) {
+    data = ppu->cpuRead(addr & 0x2000);
   }
-  if (addr >= 0x2000 && addr <= 0x3FFF) {
-    return ppu->cpuRead(addr & 0x2000);
-  }
-  return 0x00;
+
+  return data;
 }
 // Writing to the bus, currently we only have the ram so we write to it
 void NedNes::NedBus::cpuWrite(uint16_t addr, uint8_t val) {
-  if (addr >= 0x0000 && addr <= 0x1FFF) {
+
+  if (cart->cpuWrite(addr, val)) {
+  } else if (addr >= 0x0000 && addr <= 0x1FFF) {
     // mirroring the cpu write
     ram[addr & 0x7FF] = val;
-  }
-  if (addr >= 0x2000 && addr <= 0x3FFF) {
+  } else if (addr >= 0x2000 && addr <= 0x3FFF) {
     ppu->cpuWrite(addr & 0x2000, val);
   }
 }
+
 // connecting a cpu to the bus
 void NedNes::NedBus::connectCpu(NedNes::Ned6502 *_cpu) { cpu = _cpu; }
 
