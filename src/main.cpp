5443d95a1e07f73ab42929048e732e902d72af6b
Eual11 <uchihaeual11@gmail.com>
2024-08-04 22:30:44 +0300

    Testing mapper

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,9 @@
 #include "../include/NedNes.h"
 
 int main(void) {
-  NedNes::NedNesEmulator emu;
-
-  // this all is awful
-  FILE *log = fopen("../rom/tests/nedlog.log", "w");
-
-  if (!log) {
-    fprintf(stderr, "Failed to open log file");
-    return -1;
+  NedNes::NedCartrdige cart("../rom/tests/nes-test-roms/tutor/tutor.nes");
+  if (cart.imageValid()) {
+    printf("Rom Loaded\n");
   }
-  emu.nedCpu.reset();
-  emu.nedCpu.logFile = log;
-  emu.nedCpu.PC = 0x8000;
-  emu.nedCpu.PC = 0xC000;
-  emu.nedCpu.cycles = 7;
-  emu.loadRom("../rom/tests/nestest.nes", emu.nedCpu.PC);
-
-  while (!emu.nedCpu.complete) {
-    emu.nedCpu.clock();
-  }
-  printf("Job COmpleted\n");
-
   return 0;
 }
