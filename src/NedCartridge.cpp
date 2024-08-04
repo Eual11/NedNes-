5296cea116775e946422561c5fda80676cbb1d65
Eual11 <uchihaeual11@gmail.com>
2024-08-04 22:32:20 +0300

    Implementing cartiges

--- src/NedCartridge.cpp
+++ src/NedCartridge.cpp
@@ -1,48 +1,110 @@
 #include "../include/NedCartridge.h"
+#include "../include/Mapper000.h"
 #include <fstream>
+#include <memory>
 #include <stdint.h>
+#include <stdio.h>
 
 namespace NedNes {
 
 NedCartrdige::NedCartrdige(std::string filename) {
 
-  std::ifstream rom(filename);
+  std::ifstream rom(filename, std::ios::binary);
+  if (rom.is_open()) {
 
-  struct header {
-    char head[3];
-    uint8_t format;
-    uint8_t nPGR;
-    uint8_t nCHR;
-    uint8_t ctrl1;
-    uint8_t ctrl2;
-    uint8_t nRam;
-    char reserved[7];
-  };
+    struct header {
+      char head[3];
+      uint8_t format;
+      uint8_t nPGR;
+      uint8_t nCHR;
+      uint8_t ctrl1;
+      uint8_t ctrl2;
+      uint8_t nRam;
+      char reserved[7];
+    };
 
-  header hd;
+    header hd;
 
-  rom.read((char *)&hd, sizeof(header));
+    rom.read((char *)&hd, sizeof(header));
 
-  nPGRBanks = hd.nPGR;
-  nCHRBanks = hd.nCHR;
-  PGRMemory.resize(hd.nPGR * 16 * 1024);
-  CHRMemory.resize(hd.nCHR * 8 * 1024);
+    printf("Format: %X\n", hd.format);
+    printf("NPGR: %d\n", (int)hd.nPGR);
+    printf("NCHR: %d\n", (int)hd.nCHR);
+    nPGRBanks = hd.nPGR;
+    nCHRBanks = hd.nCHR;
+    PGRMemory.resize(hd.nPGR * 16 * 1024);
+    CHRMemory.resize(hd.nCHR * 8 * 1024);
 
-  // TODO: add enumrates for mirroring
-  //
-  mapperID = (hd.ctrl1 >> 4) | (hd.ctrl2 & 0xF0);
+    // TODO: add enumrates for mirroring
+    //
+    if (hd.format == 0x1A) {
+      // INES FORMAT
+      mapperID = (hd.ctrl1 >> 4) | (hd.ctrl2 & 0xF0);
+      printf("PGR Banks: %d, CHR Banks:  %d, Mapper ID: %d \n", nPGRBanks,
+             nCHRBanks, mapperID);
 
-  // skipping 512 Bytes trainer
+      switch (mapperID) {
+      case 0x00: {
+        mMapper = std::make_shared<Mapper000>(nPGRBanks, nCHRBanks);
+        break;
+      }
+      }
 
-  rom.seekg(512, std::ios::cur);
+      // skipping 512 Bytes trainer
 
-  rom.read((char *)PGRMemory.data(), PGRMemory.size());
-  rom.read((char *)CHRMemory.data(), CHRMemory.size());
+      if (hd.ctrl1 & 0x04)
+        rom.seekg(512, std::ios::cur);
+      rom.read((char *)PGRMemory.data(), PGRMemory.size());
+      rom.read((char *)CHRMemory.data(), CHRMemory.size());
 
-  valid = true;
-  // reading
-  rom.close();
+      valid = true;
+    }
+    rom.close();
+  }
+  if (!valid) {
+    fprintf(stderr, "Failed to Load Rom, Invalid ROM or Unsupported Format\n");
+  }
 }
 bool NedCartrdige::imageValid() { return valid; }
+bool NedCartrdige::cpuRead(uint16_t addr, uint8_t &data) {
+  // mapping the cpu address to the phsical program memory address in the room
+  // using the mapper
+  uint32_t mapped_addr;
+  if (mMapper->cpuMapReadAddress(addr, mapped_addr)) {
+    // reading from the program memory
+    data = PGRMemory[mapped_addr];
+  }
+  return false;
+}
 
+bool NedCartrdige::cpuWrite(uint16_t addr, uint8_t data) {
+
+  // mapping cpu address to the physical memoery address in the rom
+  uint32_t mapped_addr;
+
+  if (mMapper->cpuMapWriteAddress(addr, mapped_addr)) {
+    PGRMemory[mapped_addr] = data;
+    return true;
+  }
+  return false;
+}
+
+bool NedCartrdige::ppuRead(uint16_t addr, uint8_t &data) {
+  uint32_t mapped_addr;
+  if (mMapper->ppuMapReadAddress(addr, mapped_addr)) {
+    data = CHRMemory[mapped_addr];
+    return true;
+  }
+  return false;
+}
+bool NedCartrdige::ppuWrite(uint16_t addr, uint8_t data) {
+
+  uint32_t mapped_addr;
+
+  if (mMapper->ppuMapWriteAddress(addr, mapped_addr)) {
+    CHRMemory[mapped_addr] = data;
+    return true;
+  }
+  return false;
+}
 }; // namespace NedNes
